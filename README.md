14d76d2e9b843cab6487206b0a594e53d33bd9c3
oleesir <oleesir@gmail.com>
2020-04-20 11:40:26 +0100

    ch(integrate-coverall): implement ch-integrate-coverall-168675503
    -implement ch-integrate-coverall-168675503
    
    [Finishes #168675503]

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![Maintainability](https://api.codeclimate.com/v1/badges/1c66754453a80f1debb1/maintainability)](https://codeclimate.com/github/oleesir/authors-haven-api/maintainability) [![Test Coverage](https://api.codeclimate.com/v1/badges/1c66754453a80f1debb1/test_coverage)](https://codeclimate.com/github/oleesir/authors-haven-api/test_coverage)
 # authors-haven-api
--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,16 @@
-describe('My Test Suite', () => {
-  it('My Test Case', () => {
-    expect(true).toEqual(true);
+import request from 'supertest';
+import app from '../server/src/app';
+
+describe('app', () => {
+  it('should display authors', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).toBe(200);
+        expect(res.body.message).toBe('Authors Haven API');
+        if (err) return done(err);
+        done();
+      });
   });
 });
