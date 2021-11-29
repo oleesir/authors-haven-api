0ccf8ba07d66b7ce5ce1019f0cd72c8a831b28d4
oleesir <oleesir@gmail.com>
2021-11-29 16:32:19 +0100

    add script to setup database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -19,5 +19,11 @@ module.exports = {
 	},
 	production: {
 		use_env_variable: 'DATABASE_URL',
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
 	},
 };
