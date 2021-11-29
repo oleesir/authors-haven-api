7b2629faf4c498e0edb41f150074c70264b566ae
oleesir <oleesir@gmail.com>
2021-11-29 15:08:23 +0100

    add script to setup database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,26 +1,24 @@
 require('dotenv').config();
 
-const {
-  DB_USER, DB_HOST, DB_PASSWORD, DB_NAME, DB_NAME_TEST, DATABASE_URL, DB_DIALECT
-} = process.env;
-
+const { DB_USER, DB_HOST, DB_PASSWORD, DB_NAME, DB_NAME_TEST, DATABASE_URL, DB_DIALECT } = process.env;
 
 module.exports = {
-  development: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: DB_DIALECT
-  },
-  test: {
-    username: DB_USER || 'postgres',
-    password: DB_PASSWORD || '',
-    database: DB_NAME_TEST || 'authors_haven_test',
-    host: DB_HOST || 'localhost',
-    dialect: DB_DIALECT || 'postgres'
-  },
-  production: {
-    use_env_variable: DATABASE_URL
-  }
+	development: {
+		username: DB_USER,
+		password: DB_PASSWORD,
+		database: DB_NAME,
+		host: DB_HOST,
+		dialect: DB_DIALECT,
+	},
+	test: {
+		username: DB_USER || 'postgres',
+		password: DB_PASSWORD || '',
+		database: DB_NAME_TEST || 'authors_haven_test',
+		host: DB_HOST || 'localhost',
+		dialect: DB_DIALECT || 'postgres',
+	},
+	production: {
+		dialect: 'postgres',
+		use_env_variable: DATABASE_URL,
+	},
 };
