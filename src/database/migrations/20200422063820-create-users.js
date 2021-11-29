80a971478c18640b2e94a66dec46ed82d1bf844c
oleesir <oleesir@gmail.com>
2021-11-29 15:42:26 +0100

    add script to setup database

--- src/database/migrations/20200422063820-create-users.js
+++ src/database/migrations/20200422063820-create-users.js
@@ -1,56 +1,62 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4
-    },
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    isVerified: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    role: {
-      type: Sequelize.ENUM('user', 'admin', 'super-admin'),
-      allowNull: false,
-      defaultValue: 'user'
-    },
-    passwordResetToken: {
-      type: Sequelize.STRING,
-      allowNull: true
-    },
-    passwordTokenExpiry: {
-      type: Sequelize.DATE,
-      allowNull: true
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW
-    }
-  }),
-  down: (queryInterface) => queryInterface.dropTable('Users', { cascade: true })
+	up: async (queryInterface, Sequelize) => {
+		try {
+			await queryInterface.createTable('Users', {
+				id: {
+					allowNull: false,
+					primaryKey: true,
+					type: Sequelize.UUID,
+					defaultValue: Sequelize.UUIDV4,
+				},
+				firstName: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				lastName: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				email: {
+					type: Sequelize.STRING,
+					allowNull: false,
+					unique: true,
+				},
+				password: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				isVerified: {
+					type: Sequelize.BOOLEAN,
+					allowNull: false,
+					defaultValue: false,
+				},
+				role: {
+					type: Sequelize.ENUM('user', 'admin', 'super-admin'),
+					allowNull: false,
+					defaultValue: 'user',
+				},
+				passwordResetToken: {
+					type: Sequelize.STRING,
+					allowNull: true,
+				},
+				passwordTokenExpiry: {
+					type: Sequelize.DATE,
+					allowNull: true,
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.NOW,
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.NOW,
+				},
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	},
+	down: (queryInterface) => queryInterface.dropTable('Users', { cascade: true }),
 };
