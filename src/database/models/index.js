ece16a66e8d356bba35f92b495177c304e72819a
oleesir <oleesir@gmail.com>
2021-11-29 14:38:27 +0100

    fix sequelize config for production

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,7 +1,8 @@
-
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
+
+require('dotenv').config();
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
@@ -11,23 +12,22 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+	sequelize = new Sequelize(config.use_env_variable, config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+	sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
+fs.readdirSync(__dirname)
+	.filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+	.forEach((file) => {
+		const model = sequelize.import(path.join(__dirname, file));
+		db[model.name] = model;
+	});
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
