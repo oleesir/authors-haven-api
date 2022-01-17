125f02f190ab8fed878a6805f509235b4db10042
oleesir <oleesir@gmail.com>
2022-01-17 20:37:14 +0100

    feat: multiple image upload
    - implemented multiple image upload when creating an article

--- src/database/migrations/20200422073525-create-articles.js
+++ src/database/migrations/20200422073525-create-articles.js
@@ -1,49 +1,49 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.createTable('Articles', {
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.UUID,
-      defaultValue: Sequelize.UUIDV4
-    },
-    userId: {
-      allowNull: false,
-      type: Sequelize.UUID,
-      onDelete: 'CASCADE',
-      onUpdate: 'CASCADE',
-      references: {
-        model: 'Users',
-        key: 'id',
-      }
-    },
-    title: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    avatar: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: 'https://res.cloudinary.com/dhixv0jh9/image/upload/v1590399940/apps.24119.13561428843663101.db53229a-3063-4dd3-b18e-61ad6b3661d1_otde0c.png'
-    },
-    body: {
-      type: Sequelize.TEXT,
-      allowNull: false,
-    },
-    status: {
-      allowNull: false,
-      type: Sequelize.ENUM('draft', 'published'),
-      defaultValue: 'draft'
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: Sequelize.NOW
-    }
-  }),
-  down: (queryInterface) => queryInterface.dropTable('Articles', { cascade: true })
+	up: (queryInterface, Sequelize) =>
+		queryInterface.createTable('Articles', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.UUIDV4,
+			},
+			userId: {
+				allowNull: false,
+				type: Sequelize.UUID,
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+				references: {
+					model: 'Users',
+					key: 'id',
+				},
+			},
+			title: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			avatar: {
+				type: Sequelize.ARRAY(Sequelize.STRING),
+				allowNull: false,
+			},
+			body: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+			status: {
+				allowNull: false,
+				type: Sequelize.ENUM('draft', 'published'),
+				defaultValue: 'draft',
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.NOW,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.NOW,
+			},
+		}),
+	down: (queryInterface) => queryInterface.dropTable('Articles', { cascade: true }),
 };
--- src/validations/articleSchema.js
+++ src/validations/articleSchema.js
@@ -2,40 +2,27 @@ import { check } from 'express-validator';
 import notEmpty from '../helper/notEmpty';
 
 export default {
-  createArticleSchema: [
-    check('title')
-      .trim()
-      .exists()
-      .withMessage('title is required')
-      .custom(value => notEmpty(value, 'title field cannot be left blank'))
-      .isLength({ min: 2 })
-      .withMessage('name should be be at least 2 characters'),
-    check('status')
-      .optional()
-      .isIn(['draft', 'published'])
-      .withMessage('status can either be published or draft')
-  ],
-  getArticleSchema: [
-    check('id').isUUID(4)
-      .withMessage('invalid id')
-  ],
-  updateArticleSchema: [
-    check('id').isUUID(4)
-      .withMessage('invalid id'),
-    check('title')
-      .trim()
-      .exists()
-      .withMessage('title is required')
-      .custom(value => notEmpty(value, 'title field cannot be left blank'))
-      .isLength({ min: 2 })
-      .withMessage('name should be be at least 2 characters'),
-    check('status')
-      .optional()
-      .isIn(['draft', 'published'])
-      .withMessage('status can either be published or draft')
-  ],
-  deleteArticleSchema: [
-    check('id').isUUID(4)
-      .withMessage('invalid id')
-  ]
+	createArticleSchema: [
+		check('title')
+			.trim()
+			.exists()
+			.withMessage('title is required')
+			.custom((value) => notEmpty(value, 'title field cannot be left blank'))
+			.isLength({ min: 2 })
+			.withMessage('name should be be at least 2 characters'),
+		check('status').optional().isIn(['draft', 'published']).withMessage('status can either be published or draft'),
+	],
+	getArticleSchema: [check('id').isUUID(4).withMessage('invalid id')],
+	updateArticleSchema: [
+		check('id').isUUID(4).withMessage('invalid id'),
+		check('title')
+			.trim()
+			.exists()
+			.withMessage('title is required')
+			.custom((value) => notEmpty(value, 'title field cannot be left blank'))
+			.isLength({ min: 2 })
+			.withMessage('name should be be at least 2 characters'),
+		check('status').optional().isIn(['draft', 'published']).withMessage('status can either be published or draft'),
+	],
+	deleteArticleSchema: [check('id').isUUID(4).withMessage('invalid id')],
 };
